c25191550a60e1e33c9fa91a726e67671aec0b95
raoxiaojia <raoxiaojia@gmail.com>
2020-01-26 12:30:21 +0000

    Adding more properties on lfilled and some comments

--- theories/wasm_properties_aux.v
+++ theories/wasm_properties_aux.v
@@ -13,12 +13,41 @@ Inductive lfilledInd : nat -> lholed -> list administrative_instruction -> list 
 | LfilledRec: forall k vs n es' lh' es'' es LI,
     const_list vs ->
     lfilledInd k lh' es LI ->
-    lfilledInd (k+1) (LRec vs n es' lh' es'') es (vs ++ [ :: (Label n es' LI) ] ++ es'').
+    lfilledInd (k.+1) (LRec vs n es' lh' es'') es (vs ++ [ :: (Label n es' LI) ] ++ es'').
 
 Lemma lfilled_Ind_Equivalent: forall k lh es LI,
     lfilled k lh es LI <-> lfilledInd k lh es LI.
 Proof.
-Admitted.
+  move => k. split.
+  - move: lh es LI. induction k; move => lh es LI HFix.
+    + unfold lfilled in HFix. simpl in HFix. destruct lh.
+      * destruct (const_list l) eqn:HConst.
+        { replace LI with (l++es++l0). by apply LfilledBase.
+          (* TODO: some trivial equalities that should be solvable in one step *)
+          admit. }
+        { discriminate HFix. }
+      * discriminate HFix.
+    + unfold lfilled in HFix. simpl in HFix. destruct lh.
+      * discriminate HFix.
+      * destruct (const_list l) eqn:HConst.
+        { destruct (lfill k lh es) eqn:HLF.
+          { replace LI with (l ++ [ :: (Label n l0 l2)] ++ l1).
+          apply LfilledRec. by [].
+          apply IHk. unfold lfilled. rewrite HLF. by [].
+          (* TODO: same *)
+          admit. }
+          discriminate HFix. }
+        { discriminate HFix. }
+  - move => HLF. induction HLF.
+    + unfold lfilled. unfold lfill. rewrite H. by [].
+    + unfold lfilled. unfold lfill. rewrite H. fold lfill.
+      unfold lfilled in IHHLF. destruct (lfill k lh' es).
+      * replace LI with l.
+        { by []. }
+        (* TODO: same *)
+        admit.
+      * by [].  
+Admitted.  
 
 Lemma const_list_concat: forall vs1 vs2,
     const_list vs1 ->
@@ -43,6 +72,7 @@ Lemma lfilled_collapse1: forall n lh vs es LI l,
     exists lh', lfilledInd n lh' ((drop (length vs-l) vs) ++ es) LI.
 Proof.
   move => n lh vs es LI l HLF HConst HLen.
+  (* Comparing this proof to the original proof in Isabelle, it seems that (induction X rule: Y) in Isabelle means induction on proposition Y remembering X (in Coq). *)
   remember (vs++es) as es'. induction HLF; subst.
   - exists (LBase (vs0 ++ (take (length vs - l) vs)) es').
     (* The proof to this case should really have finished here; the below is just rearranging brackets and applying cat_take_drop and assumptions. *)
@@ -74,4 +104,16 @@ Proof.
     assert (lfilledInd 0 (LBase nil nil) es ([::] ++ es ++ [::])). apply LfilledBase. by [].
     simpl in H0. rewrite cats0 in H0. by apply H0.
   - destruct IHHLF as [x E]. auto. eexists (LRec _ _ _ _ _). apply LfilledRec. auto. by eauto.
+Qed.
+
+Lemma lfilled_deterministic: forall k lh es les les',
+    lfilledInd k lh es les ->
+    lfilledInd k lh es les' ->
+    les = les'.
+Proof.
+  move => k lh es les les' HLF. move: les'. induction HLF; subst; move => les' HLF'.
+  - by inversion HLF'.
+  - inversion HLF'; subst.
+    replace LI0 with LI. { by []. }
+    by apply IHHLF.
 Qed.
