96a494b9866626f0d82a752e159a4ff790384278
Jean Pichon <jp622@cam.ac.uk>
2020-03-11 16:02:47 +0000

    made binary parser use Ascii.ascii

--- theories/binary_tests.v
+++ theories/binary_tests.v
@@ -1,9 +1,10 @@
 Require Import binary.
 Require Import bytes.
 Require Import wasm.
-Require Import bytes.
+Require Import Byte.
 From parseque Require Import Parseque Running.
 Require Import check_toks.
 
-Definition test_unreachable : check_toks (cons #00 nil) be := MkSingleton Unreachable.
-Definition test_nop : check_toks (cons #01 nil) be := MkSingleton Nop.
+Definition test_unreachable : check_toks (cons x00 nil) be := MkSingleton Unreachable.
+
+Definition test_nop : check_toks (cons x01 nil) be := MkSingleton Nop.
--- theories/leb128.v
+++ theories/leb128.v
@@ -1,46 +1,30 @@
 (* LEB128 integer format *)
 (* TODO: size bound *)
 (* TODO: write a relational spec, and prove they correspond *)
-Require Import bytes.
 Require Import Numbers.BinNums.
 Require Import NArith.BinNat.
-From compcert Require Import Integers.
+Require Import Ascii.
+Require Import Coq.Init.Byte.
 From parseque Require Import Parseque.
 
-Fixpoint binary_of_aux (acc : list bool) (n : positive) : list bool :=
-  match n with
-  | xH => acc
-  | xI n' => binary_of_aux (cons true acc) n'
-  | xO n' => binary_of_aux (cons false acc) n'
-  end.
-
-Definition byte_two := Integers.Byte.add Integers.Byte.one Integers.Byte.one.
-Definition byte_4 := Integers.Byte.mul byte_two byte_two.
-Definition byte_8 := Integers.Byte.mul byte_4 byte_two.
-Definition byte_16 := Integers.Byte.mul byte_4 byte_4.
-Definition byte_128 := Integers.Byte.mul byte_16 byte_8.
-
-Fixpoint byte_of_bits_aux (acc : Integers.Byte.int) (bs : list bool) : Integers.Byte.int :=
-  match bs with
-  | nil => acc
-  | cons false bs' => byte_of_bits_aux (Integers.Byte.mul byte_two acc) bs'
-  | cons true bs' => byte_of_bits_aux (Integers.Byte.add (Integers.Byte.mul byte_two acc) Integers.Byte.one) bs'
-  end.
-
 (** expects 7 bits, with MSB at head *)
-Definition byte_of_bits (bs : list bool) : Integers.Byte.int :=
-  byte_of_bits_aux Integers.Byte.zero bs.
+Definition byte_of_7_bits (bs : list bool) : ascii :=
+  match bs with
+  | cons b1 (cons b2 (cons b3 (cons b4 (cons b5 (cons b6 (cons b7 nil)))))) =>
+    Ascii b7 b6 b5 b4 b3 b2 b1 false
+  | _ => Ascii false false false false false false false false
+  end.
 
 Definition rebalance acc1 acc2 b :=
-  if Nat.eqb (List.length acc2) 6 then (cons (byte_of_bits (cons b acc2)) acc1, nil)
+  if Nat.eqb (List.length acc2) 6 then (cons (byte_of_7_bits (cons b acc2)) acc1, nil)
   else (acc1, cons b acc2).
 
-Fixpoint binary_of_aux2 (acc1 : list Integers.Byte.int) (acc2 : list bool (* MSB at head *)) (n : positive) : list Integers.Byte.int :=
+Fixpoint binary_of_aux2 (acc1 : list ascii) (acc2 : list bool (* MSB at head *)) (n : positive) : list ascii :=
   match n with
   | xH =>
     let (acc1', acc2') := rebalance acc1 acc2 true in
-    let acc2'' := (* List.repeat false (7 - List.length acc2') *) acc2' in
-    cons (byte_of_bits acc2'') acc1'
+    let acc2'' := List.app (List.repeat false (7 - List.length acc2')) acc2' in
+    cons (byte_of_7_bits acc2'') acc1'
   | xI n' =>
     let (acc1', acc2') := rebalance acc1 acc2 true in
     binary_of_aux2 acc1' acc2' n'
@@ -49,41 +33,35 @@ Fixpoint binary_of_aux2 (acc1 : list Integers.Byte.int) (acc2 : list bool (* MSB
     binary_of_aux2 acc1' acc2' n'
   end.
 
-Definition decorate_msb b :=
-  Integers.Byte.add byte_128 b.
+Definition make_msb_one '(Ascii b1 b2 b3 b4 b5 b6 b7 _ : ascii) : ascii :=
+  Ascii b1 b2 b3 b4 b5 b6 b7 true.
 
-Fixpoint decorate_msbs bs :=
+Definition make_msb_of_non_first_byte_one bs :=
   match bs with
   | nil => nil
-  | cons b bs' => cons (decorate_msb b) (decorate_msbs bs')
-  end.
-
-Definition decorate_msb_of_non_first_byte bs :=
-  match bs with
-  | nil => nil
-  | cons b bs' => cons b (decorate_msbs bs')
+  | cons b bs' => cons b (List.map make_msb_one bs')
   end.
 
 (** LSB at head of list *)
-Definition binary_of (n : N) : list Integers.Byte.int :=
+Definition binary_of (n : N) : list ascii :=
   match n with
-  | N0 => cons #00 nil
-  | Npos n' => decorate_msb_of_non_first_byte (binary_of_aux2 nil nil n')
+  | N0 => cons (ascii_of_byte x00) nil
+  | Npos n' => make_msb_of_non_first_byte_one (binary_of_aux2 nil nil n')
   end.
 
-Definition encode_unsigned (n : nat) : list Integers.Byte.int :=
+Definition encode_unsigned (n : nat) : list ascii :=
   List.rev (binary_of (N.of_nat n)).
 
 Section Language.
 
   Context
-    {Toks : nat -> Type} `{Sized Toks Integers.Byte.int}
+    {Toks : nat -> Type} `{Sized Toks ascii}
     {M : Type -> Type} `{RawMonad M} `{RawAlternative M}.
   
-  Definition w_parser A n := Parser Toks Integers.Byte.int M A n.
+  Definition w_parser A n := Parser Toks ascii M A n.
 
   Definition byte_as_nat {n} : w_parser nat n :=
-  (fun x => BinIntDef.Z.to_nat (Integers.Byte.intval x)) <$> anyTok.
+  (fun x => nat_of_ascii x) <$> anyTok.
 
   Definition unsigned_end_ {n} : w_parser nat n :=
     guardM (fun n => if Nat.leb 128 n then None else Some n) byte_as_nat.
