910693ff3cf48f56844e6ab28ffd437dcb3392a7
raoxj <raoxiaojia@gmail.com>
2022-01-17 17:53:42 +0000

    init

---
+++ theories/instantiation_properties.v
@@ -0,0 +1,61 @@
+From mathcomp Require Import ssreflect ssrbool ssrnat eqtype seq.
+From ITree Require Import ITree.
+From ITree Require ITreeFacts.
+From Wasm Require Import list_extra datatypes datatypes_properties
+                         interpreter binary_format_parser operations
+                         typing opsem type_checker memory memory_list instantiation.
+From Coq Require Import BinNat.
+
+Section Host.
+
+Variable host_function : eqType.
+Let host := host host_function.
+
+Variable host_instance : host.
+
+Let store_record_eq_dec := @store_record_eq_dec host_function.
+Let store_record_eqType := @store_record_eqType host_function.
+
+Local Canonical Structure name_eqType := Eval hnf in EqType name (seq_eqMixin _).
+
+Let store_record := store_record host_function.
+Let host_state := host_state host_instance.
+
+Let store_typing := @store_typing host_function.
+
+Let external_typing := @external_typing host_function.
+
+Let executable_host := executable_host host_function.
+Variable executable_host_instance : executable_host.
+Let host_event := host_event executable_host_instance.
+
+Let instantiate := instantiate host_function host_instance.
+
+Print external_typing.
+
+Print v_ext.
+
+Print module_export_desc.
+
+Print module_export.
+
+Print extern_t.
+
+Inductive ext_typing_list: store_record -> seq module_export -> seq extern_t -> Prop :=
+| ext_typing_list_nil: forall s,
+    ext_typing_list s [::] [::]
+| ext_typing_list_cons: forall s v_exp v_exps te tes,
+    ext_typing_list s v_exps tes ->
+    external_typing s (modexp_desc v_exp) te ->
+    ext_typing_list s (v_exp :: v_exps) (te :: tes).
+
+Lemma instantiation_sound (s: store_record) m v_imps s' inst v_exps start:
+  store_typing s ->
+  instantiate s m v_imps ((s', inst, v_exps), start) ->
+  (store_typing s') /\
+  (exists C, inst_typing s' inst C) /\
+  (exists tes, ext_typing_list s' v_exps tes) /\
+  (pred_option (fun i => i < length s'.(s_funcs)) start).
+  (* /\ store_extension s s' *)
+Proof.
+Admitted.
