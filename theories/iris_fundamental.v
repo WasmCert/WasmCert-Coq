38f7de6625464393ff723530e42bac62e40f4a84
Aina Linn Georges <ageorges@cs.au.dk>
2022-02-25 17:57:07 +0100

    ftlr: binop case

--- theories/iris_fundamental.v
+++ theories/iris_fundamental.v
@@ -483,6 +483,113 @@ Section fundamental.
     }
   Qed.
 
+  (* ----------------------------------------- BINOP --------------------------------------- *)
+  
+  Lemma binop_type_agree_interp t op w1 w2 :
+    binop_type_agree t op →
+    ⊢ interp_value (Σ:=Σ) t w1 -∗
+    interp_value t w2 -∗
+    ∀ w, ⌜app_binop op w1 w2 = Some w⌝ -∗ interp_value t w.
+  Proof.
+    iIntros (Hbinop) "Hw1 Hw2".
+    inversion Hbinop.
+    { iDestruct "Hw1" as (w1') "->".
+      iDestruct "Hw2" as (w2') "->".
+      iIntros (w Happ).
+      cbn in Happ.
+      destruct op0;unfold option_map in Happ;simplify_eq;eauto.
+      { destruct s;eauto.
+        { destruct (Wasm_int.Int32.idiv_s w1' w2');simplify_eq;eauto. }
+        { destruct (Wasm_int.Int32.idiv_u w1' w2');simplify_eq;eauto. }
+      }
+      { destruct s;eauto.
+        { destruct (Wasm_int.Int32.irem_s w1' w2');simplify_eq;eauto. }
+        { destruct (Wasm_int.Int32.irem_u w1' w2');simplify_eq;eauto. }
+      }
+      { destruct s;eauto.
+        { destruct (Wasm_int.Int32.shr w1' w2');simplify_eq;eauto. }
+        { destruct (Wasm_int.Int32.ishr_u w1' w2');simplify_eq;eauto. }
+      }
+    }
+    { iDestruct "Hw1" as (w1') "->".
+      iDestruct "Hw2" as (w2') "->".
+      iIntros (w Happ).
+      destruct op0;simpl in Happ;simplify_eq;eauto.
+      { unfold option_map in Happ.
+        destruct s;eauto.
+        { destruct (Wasm_int.Int64.idiv_s w1' w2');simplify_eq;eauto. }
+        { destruct (Wasm_int.Int64.idiv_u w1' w2');simplify_eq;eauto. }
+      }
+      { unfold option_map in Happ.
+        destruct s;eauto.
+        { destruct (Wasm_int.Int64.irem_s w1' w2');simplify_eq;eauto. }
+        { destruct (Wasm_int.Int64.irem_u w1' w2');simplify_eq;eauto. }
+      }
+      { unfold option_map in Happ.
+        destruct s;eauto.
+        { destruct (Wasm_int.Int64.shr w1' w2');simplify_eq;eauto. }
+        { destruct (Wasm_int.Int64.ishr_u w1' w2');simplify_eq;eauto. }
+      }
+    }
+    { iDestruct "Hw1" as (w1') "->".
+      iDestruct "Hw2" as (w2') "->".
+      iIntros (w Happ).
+      destruct op0;simpl in Happ;simplify_eq;eauto. }
+    { iDestruct "Hw1" as (w1') "->".
+      iDestruct "Hw2" as (w2') "->".
+      iIntros (w Happ).
+      destruct op0;simpl in Happ;simplify_eq;eauto. }
+  Qed.
+    
+  Lemma typing_binop C t op : binop_type_agree t op ->
+                              ⊢ semantic_typing (HWP:=HWP) C (to_e_list [BI_binop t op]) (Tf [t; t] [t]).
+  Proof.
+    unfold semantic_typing, interp_expression.
+    iIntros (Hbinop i lh).
+    iIntros "#Hi [%Hlh_base [%Hlh_len [%Hlh_valid #Hcont]]]".
+    iIntros (f vs) "Hf #Hv".
+    iDestruct "Hf" as (locs Heq) "[#Hlocs Hf]".
+    iDestruct "Hv" as "[-> | Hv]".
+    { take_drop_app_rewrite_twice 0 1.
+      iApply (wp_wand_ctx _ _ _ (λ vs, ⌜vs = trapV⌝ ∗  ↪[frame]f)%I with "[Hf]").
+      { iApply (wp_trap_ctx with "[$]");auto. }
+      iIntros (v0) "[? ?]". iFrame. iExists _. iFrame. }
+    { iDestruct "Hv" as (ws ->) "Hv".
+      iDestruct (big_sepL2_length with "Hv") as %Hlen.
+      destruct ws as [|w1 ws];[done|destruct ws as [|w2 ws];[done|destruct ws;[|done]]].
+      iSimpl in "Hv".
+      iDestruct "Hv" as "[Hv1 [Hv2 _]]".
+      iDestruct "Hcont" as "[_ Hcont]".
+
+      destruct (app_binop op w1 w2) eqn:Hsome.
+      
+      { iSimpl. take_drop_app_rewrite 3.
+        iApply (wp_seq_can_trap_ctx _ _ _ (λ v, ⌜v = immV [from_option id w1 (app_binop op w1 w2)]⌝)%I).
+        iFrame.
+        iSplitR;[done|].
+        iSplitR;[auto|].
+        iSplitR.
+        { iIntros "Hf". iApply (wp_binop with "Hf");eauto.
+          rewrite Hsome. auto. }
+        { iIntros (w0) "[-> Hf]".
+          rewrite app_nil_r.
+          iApply "Hcont".
+          { iRight. iExists [v]. rewrite Hsome. iSplit;auto.
+            iSimpl. iSplit;auto.
+            iApply (binop_type_agree_interp with "Hv1 Hv2");eauto. }
+          { iExists _. iFrame. eauto. }
+        }
+      }
+
+      iApply (wp_wand_ctx _ _ _ (λ vs, ⌜vs = trapV⌝ ∗  ↪[frame]f)%I with "[Hf]").
+      { iSimpl. take_drop_app_rewrite 3.
+        iApply wp_seq_trap_ctx. iFrame.
+        iIntros "Hf".
+        iApply wp_binop_failure;auto. }
+      { iIntros (v) "[-> Hf]". iSplitR;[|eauto]. by iLeft. }
+    }
+  Qed.
+
   (* ------------------------------------------ BR ----------------------------------------- *)
   
   Lemma typing_br C i t1s ts t2s : ssrnat.leq (S i) (length (tc_label C)) ->
@@ -736,7 +843,7 @@ Section fundamental.
     induction 1.
     { by apply typing_const. }
     { by apply typing_unop. }
-    { admit. }
+    { by apply typing_binop. }
     { admit. }
     { admit. }
     { admit. }
