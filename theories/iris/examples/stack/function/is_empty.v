b70edd8fc8ba7a54169b4a669bc0443aad7e8acd
raoxj <raoxiaojia@gmail.com>
2022-10-26 19:16:53 +0100

    is_empty

--- theories/iris/examples/stack/function/is_empty.v
+++ theories/iris/examples/stack/function/is_empty.v
@@ -22,10 +22,9 @@ Section code.
 
 Definition is_empty :=
   validate_stack 0 ++
+  validate_stack_bound 0 ++
   [
     BI_get_local 0 ;
-    i32const 4 ;
-    BI_binop T_i32 (Binop_i BOI_add) ;
     BI_get_local 0 ;
     BI_load T_i32 None N.zero N.zero ;
     BI_relop T_i32 (Relop_i ROI_eq)
@@ -40,7 +39,6 @@ Section specs.
 Lemma spec_is_empty f0 n v s E: 
   ⊢ {{{ ⌜ f0.(f_inst).(inst_memory) !! 0 = Some n ⌝ ∗
         ⌜ (f_locs f0) !! 0 = Some (value_of_int v) ⌝ ∗ 
-        ⌜ (0 <= v <= Wasm_int.Int32.max_unsigned - 4 - length s * 4)%Z ⌝ ∗ 
         ↪[frame] f0 ∗
         isStack v s n }}}
     to_e_list is_empty @  E
@@ -49,7 +47,9 @@ Lemma spec_is_empty f0 n v s E:
                   (k = 0 /\ s <> []) ⌝ ∗
            ∃ f1, ↪[frame] f1 ∗ ⌜ f_inst f0 = f_inst f1 ⌝}}}.
 Proof.
-  iIntros "!>" (Φ) "(%Hinst & %Hlocv & %Hv & Hf & Hstack) HΦ" => /=.
+  iIntros "!>" (Φ) "(%Hinst & %Hlocv & Hf & Hstack) HΦ" => /=.
+
+  
   rewrite separate4.
   iApply wp_seq.
   instantiate (1 := λ x,  (⌜ x = immV [] ⌝ ∗ isStack v s n ∗ ↪[frame] f0)%I).
@@ -57,6 +57,15 @@ Proof.
   iSplitL "Hstack Hf"; first by iApply (is_stack_valid with "[$Hstack $Hf]").
   iIntros (w) "(-> & Hstack & Hf)".
   simpl.
+
+  rewrite separate3.
+  iApply wp_seq.
+  instantiate (1 := λ x,  (⌜ x = immV [] ⌝ ∗ isStack v s n ∗ ↪[frame] f0)%I).
+  iSplitR; first by iIntros "(%H & _)".
+  iSplitL "Hstack Hf"; first by iApply (is_stack_bound_valid with "[$Hstack $Hf]").
+  iIntros (w) "(-> & Hstack & Hf)".
+  simpl.
+  
   rewrite separate1.
   iApply wp_seq.
   instantiate (1 := λ x, (⌜ x = immV [value_of_int v] ⌝ ∗ ↪[frame] f0)%I).
@@ -65,34 +74,12 @@ Proof.
   iSplitL "Hf".
   - iApply (wp_get_local with "[] [$Hf]") => //=.
   - iIntros (w) "[-> Hf]".
-    unfold of_val, fmap, list_fmap .
-    rewrite - separate1.
-    rewrite separate3.
-    iApply wp_seq.
-    instantiate (1 := λ x, (⌜ x = immV [value_of_int (v + 4)%Z] ⌝ ∗ ↪[frame] f0)%I).
-    iSplitR.
-    by iIntros "[%Habs _]".
-    iSplitL "Hf".
-  - iApply (wp_binop with "Hf") => //=.
-    unfold Wasm_int.Int32.iadd, Wasm_int.Int32.add.
-    rewrite Wasm_int.Int32.unsigned_repr.
-    rewrite Wasm_int.Int32.unsigned_repr.
-    done.
-    unfold Wasm_int.Int32.max_unsigned.
-    unfold Wasm_int.Int32.modulus.
-    unfold Wasm_int.Int32.wordsize.
-    unfold Integers.Wordsize_32.wordsize.
-    replace (two_power_nat 32) with 4294967296%Z ; last done.
-    lia.
-    lia.
-  - iIntros (w) "[-> Hf]".
-    unfold of_val, fmap, list_fmap.
     rewrite - separate1.
     rewrite separate2.
     iApply wp_seq.
-    instantiate (1 := λ x, (⌜ x = immV [value_of_int (v + 4)%Z ; value_of_int v] ⌝
-                                       ∗ ↪[frame] f0)%I).
-    iSplitR ; first by iIntros "[%Habs _]".
+    instantiate (1 := λ x, (⌜ x = immV [value_of_int v%Z; value_of_int v%Z] ⌝ ∗ ↪[frame] f0)%I).
+    iSplitR.
+    by iIntros "[%Habs _]".
     iSplitL "Hf".
   - rewrite separate1.
     iApply wp_val_app => //=.
@@ -100,20 +87,19 @@ Proof.
     by iIntros "!> [%Habs _]".
     iApply (wp_get_local with "[] [$Hf]") => //=.
   - iIntros (w) "[-> Hf]".
-    unfold of_val, fmap, list_fmap.
     rewrite - separate2.
     rewrite separate3.
     iApply wp_seq.
-    instantiate ( 1 := λ x, ((((⌜ x = immV [value_of_int (v + 4)%Z ;
-                                            value_of_int (v + 4 + length s * 4)%Z] ⌝
+    instantiate ( 1 := λ x, ((((⌜ x = immV [value_of_int v%Z ;
+                                            value_of_int (v + length s * 4)%Z] ⌝
                                           ∗ [∗ list] i↦w ∈ s,
-                                N.of_nat n ↦[i32][ Z.to_N (v + 4 + length s * 4 - 4 - 4 * i)] w) ∗
+                                N.of_nat n ↦[i32][ Z.to_N (v + length s * 4 -  4 * i)] w) ∗
 
-                                                                                                 (∃ bs, ⌜ (Z.of_nat (length bs) = two16 - 4 - length s * 4)%Z ⌝ ∗ N.of_nat n↦[wms][Z.to_N (v + 4 + length s * 4)]bs))
-                                           ∗  N.of_nat n↦[wms][(Wasm_int.N_of_uint i32m (Wasm_int.int_of_Z i32m v) + N.zero)%N]bits (value_of_int (v + 4 + length s * 4)) )
+                                                                                                 (∃ bs, ⌜ (Z.of_nat (length bs) = two16 - 4 - length s * 4)%Z ⌝ ∗ N.of_nat n↦[wms][Z.to_N (v + length s * 4) + 4]bs))
+                                           ∗  N.of_nat n↦[wms][(Wasm_int.N_of_uint i32m (Wasm_int.int_of_Z i32m v) + N.zero)%N]bits (value_of_int (v + length s * 4)) )
                                            ∗ ↪[frame] f0)%I).
     iSplitR ; first by iIntros "[[[[%Habs _ ]_ ] _] _]".
-    iDestruct "Hstack" as "(%Hdiv & %Hlen & Hv & Hs & Hrest)". 
+    iDestruct "Hstack" as "(%Hdiv & %Hvb & %Hlen & Hv & Hs & Hrest)". 
     iSplitR "HΦ".
   - rewrite separate1.
     iApply wp_val_app => //.
@@ -126,7 +112,7 @@ Proof.
     iFrame.
     rewrite N.add_0_r.
     rewrite Wasm_int.Int32.Z_mod_modulus_eq.
-    rewrite Z.mod_small ; last by unfold Wasm_int.Int32.max_unsigned in Hv ; lia.
+    rewrite Z.mod_small ; last by unfold ffff0000 in Hvb; rewrite u32_modulus ; lia.
     iDestruct (i32_wms with "Hv") as "Hv" => //.
   - iIntros (w) "[[[[->  Hs] Hrest] Hp] Hf]".
     iAssert (isStack v s n)%I with "[Hrest Hp Hs]" as "Hstack".
@@ -135,7 +121,7 @@ Proof.
     rewrite N.add_0_r.
     simpl.
     rewrite Wasm_int.Int32.Z_mod_modulus_eq.
-    rewrite Z.mod_small ; last by unfold Wasm_int.Int32.max_unsigned in Hv ; lia.
+    rewrite Z.mod_small ; last by unfold ffff0000 in Hvb; rewrite u32_modulus ; lia.
     repeat iSplit => //=.
     iApply i32_wms => //.
   - unfold of_val, fmap, list_fmap.
@@ -143,8 +129,7 @@ Proof.
     iApply wp_wand_r.
     iSplitL "Hf".
     iApply (wp_relop with "Hf") => //=.
-    instantiate (1 := λ x, ⌜ x = immV [VAL_int32 (wasm_bool (Wasm_int.Int32.eq (Wasm_int.Int32.repr (v + 4)) (Wasm_int.Int32.repr (v + 4 + length s * 4))))] ⌝%I).
-    done.
+    by instantiate (1 := λ x, ⌜ x = immV _ ⌝%I).
   - iIntros (w) "[-> Hf]".
     iApply "HΦ".
     iExists _.
@@ -152,26 +137,29 @@ Proof.
     iPureIntro.
     unfold value_of_int.
     unfold wasm_bool.
-    instantiate (1 := if Wasm_int.Int32.eq (Wasm_int.Int32.repr (v + 4))
-                                           (Wasm_int.Int32.repr (v + 4 + length s * 4))
+    instantiate (1 := if Wasm_int.Int32.eq (Wasm_int.Int32.repr v)
+                                           (Wasm_int.Int32.repr (v + length s * 4))
                       then 1%Z else 0%Z).
-    destruct (Wasm_int.Int32.eq (Wasm_int.Int32.repr (v + 4))
-                                (Wasm_int.Int32.repr (v + 4 + length s * 4))) => //=.
-  - iFrame.
-    iSplit.
+    remember (Wasm_int.Int32.eq (Wasm_int.Int32.repr v)
+                                (Wasm_int.Int32.repr (v + length s * 4))) as eqv.
+    rewrite - Heqeqv.
+    by destruct eqv => //=.
+  - iFrame "Hstack".
+    iSplit; last by iExists f0; iFrame "Hf".
     iPureIntro.
     destruct s.
     left.
     split => //=.
-    replace (v + 4 + 0%nat * 4)%Z with (v + 4)%Z ; last lia.
+    replace (v + 0%nat * 4)%Z with v%Z ; last lia.
     by rewrite Wasm_int.Int32.eq_true.
     right.
     split => //=.
     rewrite Wasm_int.Int32.eq_false => //=.
     intro.
-    simpl in Hv.
-    apply Wasm_int.Int32.repr_inv in H ; unfold Wasm_int.Int32.max_unsigned in Hv ; try lia.
-    iExists _ ; by iFrame.
+    unfold ffff0000 in Hvb.
+    simpl in Hlen.
+    unfold two14 in Hlen.
+    apply Wasm_int.Int32.repr_inv in H ; try rewrite u32_modulus; by lias.
 Qed.
     
 End specs.
