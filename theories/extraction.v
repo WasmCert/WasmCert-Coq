a6ba8432bb6ad91a3c2aa927dc0498cf28a4d907
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2023-05-29 19:51:24 +0100

    use a fork of CompCert with int renamed to int_compcert to fix ExtrOcamlNatInt

--- theories/extraction.v
+++ theories/extraction.v
@@ -14,6 +14,7 @@ From Wasm Require Import
 From Coq Require Import
   extraction.ExtrOcamlBasic
   extraction.ExtrOcamlNatInt
+  extraction.ExtrOcamlZInt
   extraction.ExtrOcamlString.
 
 Extraction Language OCaml.
