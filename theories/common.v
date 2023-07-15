30af82c931d22b0adc3884093a060c2ef55a172c
Wolfgang Meier <womeier@posteo.de>
2023-07-15 09:32:24 +0200

    readd remaining induction<n> definitions

--- theories/common.v
+++ theories/common.v
@@ -1036,6 +1036,47 @@ Section Inductions.
     do 2 (destruct n as [|n]; auto).
     apply IH. auto.
   Qed.
+
+  Lemma rect3 :
+    (forall n, P n -> P (3 + n)) ->
+    forall n, P n.
+  Proof.
+    intros IH n. strong induction n.
+    do 3 (destruct n as [|n]; auto).
+    apply IH. auto.
+  Qed.
+
+  Lemma rect4 :
+    (forall n, P n -> P (4 + n)) ->
+    forall n, P n.
+  Proof.
+    intros IH n. strong induction n.
+    do 4 (destruct n as [|n]; auto).
+    apply IH. auto 10.
+  Qed.
+
+  Lemma rect5 :
+    (forall n, P n -> P (5 + n)) ->
+    forall n, P n.
+  Proof.
+    intros IH n. strong induction n.
+    do 5 (destruct n as [|n]; auto).
+    apply IH. auto 10.
+  Qed.
+
+  Lemma rect6 :
+    (forall n, P n -> P (6 + n)) ->
+    forall n, P n.
+  Proof.
+    intros IH n. strong induction n.
+    do 6 (destruct n as [|n]; auto).
+    apply IH. auto 10.
+  Qed.
+
 End Inductions.
 
 Definition induction2 (P : nat -> Prop) := @rect2 P.
+Definition induction3 (P : nat -> Prop) := @rect3 P.
+Definition induction4 (P : nat -> Prop) := @rect4 P.
+Definition induction5 (P : nat -> Prop) := @rect5 P.
+Definition induction6 (P : nat -> Prop) := @rect6 P.
