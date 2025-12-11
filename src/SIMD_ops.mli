281719896461d023e792494ee7aebfbcafedfcec
raoxiaojia <raoxiaojia@gmail.com>
2025-12-11 03:15:10 -0800

    fixed extraction type error

--- src/SIMD_ops.mli
+++ src/SIMD_ops.mli
@@ -1,9 +1,9 @@
-val app_vunop_str : Big_int_Z.big_int -> string -> string
+val app_vunop_str : int -> string -> string
 
-val app_vbinop_str : Big_int_Z.big_int * (Big_int_Z.big_int list) -> string -> string -> string
+val app_vbinop_str : int * (int list) -> string -> string -> string
 
-val app_vternop_str : Big_int_Z.big_int -> string -> string -> string -> string
+val app_vternop_str : int -> string -> string -> string -> string
 
-val app_vtestop_str : Big_int_Z.big_int -> string -> string
+val app_vtestop_str : int -> string -> string
 
-val app_vshiftop_str : Big_int_Z.big_int -> string -> string -> string
+val app_vshiftop_str : int -> string -> string -> string
